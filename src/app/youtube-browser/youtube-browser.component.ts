a4029982d824b67ec4427d7a7a6e5b2f97e800ac
Mirko Atanasov <mirko.atanasov@dh.com>
2017-11-21 01:55:18 -0500

    fixing some wierd errors

--- src/app/youtube-browser/youtube-browser.component.ts
+++ src/app/youtube-browser/youtube-browser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YouTubeSearchResult } from 'app/youtube-video-browser/search-result';
 
 @Component({
   selector: 'app-youtube-browser',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class YoutubeBrowserComponent implements OnInit {
   
   isSearching : boolean
-
+  videoResults : any;
   constructor() { }
 
   ngOnInit() {
